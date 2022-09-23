fd606bec24b052a73193f90163120f0e50994ba0
weishentan1 <101373731+weishentan1@users.noreply.github.com>
2022-09-23 20:16:17 +1000

    Able to view backend data in frontend webpage

--- backend/thanos/inventorySystem/inventory/urls.py
+++ backend/thanos/inventorySystem/inventory/urls.py
@@ -1,6 +1,26 @@
 from django.urls import path
 from .views import index
+from inventory import views
+from django.urls import re_path as url
+from django.contrib import admin
+
+# upload image
+from django.conf.urls.static import static
+from django.conf import settings
+
+admin.site.site_header = 'Java Jivers Database'
+admin.site.site_title = 'Java Jivers administration'
 
 urlpatterns = [
-    path('a/', index, name="index"),
-]
+    path("", views.index, name="inventory-index"),
+    path("manager/", views.manager, name='inventory-manager'),
+    path("product/", views.product, name='inventory-product'),
+    path("area/", views.area, name='inventory-area'),
+    url(r'^inventory$', views.itemApi),
+    # ([0-9]+)$ will pass a number here
+    url(r'^inventory/([0-9]+)$', views.itemApi),
+    # if can use the _id field in mongodb
+    # url(r'^user/(?P<id>[\w.@+-]+)$', views.userApi)
+    # (?P<id>[\w.@+-]+) this will pass a string include the char '.' '@' '+' and '-'
+    url(r'^item/savefile', views.SaveFile)
+] +static(settings.MEDIA_URL, documnet_root = settings.MEDIA_ROOT)
--- backend/thanos/inventorySystem/inventory/views.py
+++ backend/thanos/inventorySystem/inventory/views.py
@@ -1,5 +1,80 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from django.http.response import JsonResponse
+
+from inventory.models import Item
+from inventory.serializers import itemSerializer
+
+# allow upload file
+from django.core.files.storage import default_storage
 
 # Create your views here.
 def index(request):
-    return render(request, "t_inventory/index.html")
+    context = {
+        "title": "Home Page"
+    }
+    return render(request, "t_inventory/index.html", context=context)
+
+def manager(request):
+    ##return HttpResponse('Manager mode')
+    context = {
+        "title": "Manager Page"
+    }
+    return render(request, "t_inventory/manager.html", context=context)
+
+def product(request):
+    ##return HttpResponse('Manager mode')
+    context = {
+        "title": "Product Page"
+    }
+    return render(request, "t_inventory/product.html", context=context)
+
+def area(request):
+    ##return HttpResponse('Manager mode')
+    context = {
+        "title": "Area Page"
+    }
+    return render(request, "t_inventory/area.html", context=context)
+
+@csrf_exempt
+def itemApi(request, id=0):
+
+    # get all the Item
+    if request.method=='GET':
+        item = Item.objects.all()
+        item_serializer = itemSerializer(item, many=True)
+        return JsonResponse(item_serializer.data,safe=False)
+    
+    # add Item
+    elif request.method=='POST':
+        item_data = JSONParser().parse(request)
+        item_serializer = itemSerializer(data = item_data)
+        if item_serializer.is_valid():
+            item_serializer.save()
+            return JsonResponse("Added Successfully", safe = False)
+        print(item_serializer.errors)
+        return JsonResponse("Failed to add", safe = False)
+
+    # edit Item's data
+    elif request.method=='PUT':
+        item_data = JSONParser().parse(request)
+        item = Item.objects.get(idItem = item_data['idItem'])
+        item_serializer = itemSerializer(item, data = item_data)
+        if item_serializer.is_valid():
+            item_serializer.save()
+            return JsonResponse("Updated Successfully", safe = False)
+        print(item_serializer.errors)
+        return JsonResponse("Failed to update", safe = False)
+
+    # delete user through UserId, not the objectId
+    elif request.method=='DELETE':
+        item = item.objects.get(idItem=id)
+        item.delete()
+        return JsonResponse("Delete Successfully", safe = False)
+
+@csrf_exempt
+def SaveFile(request):
+    file = request.FILES['file']
+    file_name = default_storage.save(file.name,file)
+    return JsonResponse(file_name, safe=False)
