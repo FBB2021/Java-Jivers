7cd818452944947d2d703c51f16ae6c0e0472d35
unknown <jordanking271@outlook.com>
2022-10-10 20:09:11 +1100

    Finished adding login functionality to frontend. Mostly in the index.js file, but should probably be in a seperate file in the Store folder called authentication. Cannot test if it works because cannot get backend running on my local computer

--- Frontend/src/Store/index.js
+++ Frontend/src/Store/index.js
@@ -11,6 +11,7 @@ const store = new Vuex.Store({
         isAuthenticated: false,
         isAdmin: false,
         user: "",
+        password: "",
         csrf: "",
     },
     mutations: {
@@ -34,52 +35,112 @@ const store = new Vuex.Store({
         },
         set_csrf(state, csrf) {
             state.csrf = csrf;
-        }
+        },
+        set_password(state, password) {
+            state.password = password;
+        },
     },
     actions: {
         logout(context) {
-            context.commit("set_isAuthenticated", false);
-            context.commit("set_isAdmin", false);
-            context.commit("set_user", null);
+            axios
+                .fetch("http://localhost:8000/Login/logout", {
+                    credentials: "include",
+                })
+                .then(this.this.is_response_ok)
+                .then((data) => {
+                    console.log(data);
+                    context.commit("set_isAuthenticated", false);
+                    context.commit("set_isAdmin", false);
+                    context.commit("set_user", null);
+                    context.commit("set_password", null);
+                    this.fetch_CSRF();
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
         },
-        async login(context, user) {
-            await axios.post("login", user);
-            context.commit("set_user", user.get("username"));
-            context.commit("set_isAuthenticated", true);
-            context.commit("set_isAdmin", true);
+        login(context, user_data) {
+            axios
+                .fetch("https://java-jivers-ims.herokuapp.com/Login/login", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "X-CSRFToken": this.state.csrf,
+                    },
+                    credentials: "include",
+                    body: JSON.stringify({
+                        username: user_data.username,
+                        password: user_data.password,
+                    }),
+                })
+                .then(this.is_response_ok)
+                .then((data) => {
+                    console.log(data);
+                    context.commit("set_isAuthenticated", true);
+                    context.commit("set_isAdmin", true);
+                    context.commit("set_user", null);
+                    context.commit("set_password", null);
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
         },
-        component_did_mount(context){
+        component_did_mount(context) {
             context.commit("get_session");
         },
         // https://github.com/duplxey/django-spa-cookie-auth/blob/master/django_react_cross_origin/frontend/src/App.js
-        fetch_CSRF(context){
-            axios.fetch("https://java-jivers-ims.herokuapp.com/Login/csrf",{
-                credentials: "includes",
-            })
-            .then((res) => {
-                context.commit("set_csrf", res.headers.get("X-CSRFToken"));
-            })
-            .catch((err) => {
-                console.log(err);
-            })
+        fetch_CSRF(context) {
+            axios
+                .fetch("https://java-jivers-ims.herokuapp.com/Login/csrf", {
+                    credentials: "includes",
+                })
+                .then((res) => {
+                    context.commit("set_csrf", res.headers.get("X-CSRFToken"));
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
         },
-        fetch_session(context){
-            axios.fetch("https://java-jivers-ims.herokuapp.com/Login/session/",{
-                credentials: "includes",
-            })
-            .then((res) => res.json())
-            .then((data) => {
-                if(data.isAuthenticated){
-                    context.commit("set_isAuthenticated", true);
-                }
-                else{
-                    context.commit("set_isAuthenticated", false);
-                    this.fetch_CSRF();
-                }
-            })
-            .catch((err) => {
-                console.log(err);
-            })
+        fetch_session(context) {
+            axios
+                .fetch("https://java-jivers-ims.herokuapp.com/Login/session/", {
+                    credentials: "includes",
+                })
+                .then((res) => res.json())
+                .then((data) => {
+                    if (data.isAuthenticated) {
+                        context.commit("set_isAuthenticated", true);
+                    } else {
+                        context.commit("set_isAuthenticated", false);
+                        this.fetch_CSRF();
+                    }
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        },
+        whoami(context) {
+            axios
+                .fetch("https://java-jivers-ims.herokuapp.com/Login/whaomi/", {
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    credentials: "include",
+                })
+                .then((res) => res.json())
+                .then((data) => {
+                    console.log("You are logged in as: " + data.username);
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        },
+        is_response_ok(response) {
+            if (response.status >= 200 && response.status <= 299) {
+                return response.json();
+            } else {
+                throw Error(response.statusText);
+            }
         },
     },
     getters: {
@@ -94,74 +155,5 @@ const store = new Vuex.Store({
         },
     },
 });
-
-  whoami = () => {
-    fetch("http://localhost:8000/api/whoami/", {
-      headers: {
-        "Content-Type": "application/json",
-      },
-      credentials: "include",
-    })
-    .then((res) => res.json())
-    .then((data) => {
-      console.log("You are logged in as: " + data.username);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  }
-
-  handlePasswordChange = (event) => {
-    this.setState({password: event.target.value});
-  }
-
-  handleUserNameChange = (event) => {
-    this.setState({username: event.target.value});
-  }
-
-  isResponseOk(response) {
-    if (response.status >= 200 && response.status <= 299) {
-      return response.json();
-    } else {
-      throw Error(response.statusText);
-    }
-  }
-
-  login = (event) => {
-    event.preventDefault();
-    fetch("http://localhost:8000/api/login/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "X-CSRFToken": this.state.csrf,
-      },
-      credentials: "include",
-      body: JSON.stringify({username: this.state.username, password: this.state.password}),
-    })
-    .then(this.isResponseOk)
-    .then((data) => {
-      console.log(data);
-      this.setState({isAuthenticated: true, username: "", password: "", error: ""});
-    })
-    .catch((err) => {
-      console.log(err);
-      this.setState({error: "Wrong username or password."});
-    });
-  }
-
-  logout = () => {
-    fetch("http://localhost:8000/api/logout", {
-      credentials: "include",
-    })
-    .then(this.isResponseOk)
-    .then((data) => {
-      console.log(data);
-      this.setState({isAuthenticated: false});
-      this.getCSRF();
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  };
 
 export default store;
