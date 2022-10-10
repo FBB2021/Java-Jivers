7fab2de4b65ef3d0edf6604d0f163a4f523c4945
FBB2021 <89294095+FBB2021@users.noreply.github.com>
2022-10-10 23:05:22 +1100

    An existing user can now change his infomation (including password)
    An existing user can now change his infomation (including password)
    
    Next Move: If there is duplicate user found in database when creating user, throw an exception rather than give database error.

--- backend/inventorySystem/Login/views.py
+++ backend/inventorySystem/Login/views.py
@@ -9,7 +9,8 @@ from django.http.response import JsonResponse
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 
-
+# hashing raw user instance password
+from django.contrib.auth.hashers import make_password
 
 # allow upload file
 from django.core.files.storage import default_storage
@@ -88,3 +89,21 @@ class UserViewSet(viewsets.ModelViewSet):
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 
+    def update(self, request, *args, **kwargs):
+        username = request.data['username']
+        pwd_raw = request.data['password']
+        pwd_hashed = make_password(pwd_raw)
+        request.data['password'] = pwd_hashed
+
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
