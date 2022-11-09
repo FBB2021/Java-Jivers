27035fbdd2c0910c7a071a6a7a3383761bd85adb
ziyuh113 <ziyuh113@gmail.com>
2022-11-10 10:01:02 +1100

    Update TableList.vue

--- Frontend/src/pages/Admin/TableList.vue
+++ Frontend/src/pages/Admin/TableList.vue
@@ -13,19 +13,14 @@
 </template>
 
 <script>
-import Axios from "axios";
-const backendUrl = "https://java-jivers-ims.herokuapp.com/item";
-
 
 export default {
-
   name: "Heatmap",
   data: function () {
     return {
       chartOptions: {
         dataLabels: {
           enabled: false,
-          tableData: []
         },
         chart: {
         
@@ -44,116 +39,44 @@ export default {
       series: [
         {
           name: "A",
-          data: this.generateDataA(10, {
+          data: this.generateData(10, {
             min: 0,
-            max: 0,
+            max: 21,
           }),
         },
         {
           name: "B",
-          data: this.generateDataB(10, {
+          data: this.generateData(10, {
             min: 0,
-            max: 0,
+            max: 21,
           }),
         },
         {
           name: "C",
-          data: this.generateDataC(10, {
+          data: this.generateData(10, {
             min: 0,
-            max: 0,
+            max: 21,
           }),
         },
         {
           name: "D",
-          data: this.generateDataD(10, {
+          data: this.generateData(10, {
             min: 0,
-            max: 0,
+            max: 21,
           }),
         },
       ],
     };
   },
-  async mounted(){
-        await Axios.get(backendUrl).then((response) => {
-                this.tableData = response.data;
-            });
+  mounted(){
 
-            console.log(this.tableData)
-          },
+  },
   methods: {
 
-    generateDataA(count, yrange) {
-      console.log("tada!")
-      console.log(this.tableData)
-      // var i = 0;
-      // var series = [];
-      // while (i < count) {
-      //   var x = `${i}`.toString();
-
-      //   console.log("A"+x == "A1")
-      //   for (var i = 0; i < this.tableData.length; i++) {
-      //           if( "A"+x == this.tableData[i].nameLocation){
-      //             var y = this.tableData[i].quantity
-      //           }else{
-      //             var y =
-      //             Math.floor(Math.random() * (yrange.max - yrange.min + 1)) +
-      //             yrange .min;
-      //           }
-      //       }
-
-
-      //   series.push({
-      //     x: x,
-      //     y: y,
-      //   });
-      //   i++;
-      // }
-      // console.log(series)
-      // return series;
-    },
-    generateDataB(count, yrange) {
+    generateData(count, yrange) {
       var i = 0;
       var series = [];
       while (i < count) {
-
-        var x = `${i}`.toString();
-        var y =
-          Math.floor(Math.random() * (yrange.max - yrange.min + 1)) +
-          yrange.min;
-
-        series.push({
-          x: x,
-          y: y,
-        });
-        i++;
-      }
-      console.log(series)
-      return series;
-    },
-    generateDataC(count, yrange) {
-      var i = 0;
-      var series = [];
-      while (i < count) {
-
-        var x = `${i}`.toString();
-        var y =
-          Math.floor(Math.random() * (yrange.max - yrange.min + 1)) +
-          yrange.min;
-
-        series.push({
-          x: x,
-          y: y,
-        });
-        i++;
-      }
-      console.log(series)
-      return series;
-    },
-    generateDataD(count, yrange) {
-      var i = 0;
-      var series = [];
-      while (i < count) {
-
         var x = `${i}`.toString();
         var y =
           Math.floor(Math.random() * (yrange.max - yrange.min + 1)) +
